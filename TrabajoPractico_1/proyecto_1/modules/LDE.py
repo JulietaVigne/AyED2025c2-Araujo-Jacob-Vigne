e706b2c7fc980ae365b01f73690f523b600b2983
inearaujoezc <ine.araujoezcurra@gmail.com>
2025-09-02 19:56:50 -0300

    codigo actualizado, falta ver al metodo add

--- TrabajoPractico_1/proyecto_1/modules/LDE.py
+++ TrabajoPractico_1/proyecto_1/modules/LDE.py
@@ -59,10 +59,14 @@ class ListaDobleEnlazada: #creamos la clase LDE
     
     def extraer(self,posicion=None):
         if posicion is None:
-            posicion=self.tamanio-1   
+            posicion=self.tamanio-1
+        elif posicion < 0:
+            posicion=self.tamanio+posicion
+            if posicion <0: 
+                raise Exception("Posición inválida")    
         if self.esta_vacia():
             raise Exception("La lista está vacía")
-        if posicion<0 or posicion >self.tamanio:
+        if posicion >self.tamanio:
             raise Exception("Posición inválida") 
         if posicion==0:
             nodo_a_extraer = self.cabeza
@@ -72,7 +76,7 @@ class ListaDobleEnlazada: #creamos la clase LDE
                self.cabeza.anterior = None
             else:
                self.cola = None
-        elif posicion==self.tamanio:
+        elif posicion==self.tamanio-1:  #extrae la cola osea extrae extremos
             nodo_a_extraer = self.cola
             dato = nodo_a_extraer.dato
             self.cola = nodo_a_extraer.anterior
@@ -98,13 +102,13 @@ class ListaDobleEnlazada: #creamos la clase LDE
             copia.agregar_al_final(actual.dato)
             actual=actual.siguiente
         return copia
+    
     def invertir(self): #falta ver las exepciones
         actual=self.cabeza
         while actual is not None:
             actual.siguiente,actual.anterior=actual.anterior,actual.siguiente
             actual=actual.anterior
         self.cabeza,self.cola=self.cola,self.cabeza #para hacerlo a la vez
-        
         
     def concatenar (self,lista): #falta ver las exepciones
         # Recibe una lista como argumento y retorna la lista actual 
@@ -119,16 +123,19 @@ class ListaDobleEnlazada: #creamos la clase LDE
         self.cola.siguiente=lista.cabeza
         lista.cabeza.anterior=self.cola
         self.cola=lista.cola
-        self.cabeza.anterior=None
+        lista.cabeza.anterior=None
         self.cola.siguiente=None
         self.tamanio+=lista.tamanio
         return self
+    
     def __len__(self):#PREGUNTAR PARAMETRO
         return self.tamanio
+    
     def __add__(self,lista1):
         lista_nueva=ListaDobleEnlazada()
-        lista_nueva= self + lista1
+        lista_nueva= self.concatenar(lista1)
         return (lista_nueva)
+    
     def __iter__(self): #PREGUNTAR PARAMTERO
         actual=self.cabeza
         lista=[]

90f9f1de4ff0fbd3a0f7952bcd05a68bc8d298a6
julietavigne <julivigne@gmail.com>
2025-10-01 13:22:51 -0300

    subimos tp2

---
+++ TP2/proyecto1/proyecto_1/modules/modulo1.py
@@ -0,0 +1,69 @@
+# módulo para organizar funciones o clases utilizadas en nuestro proyecto
+# Crear tantos módulos como sea necesario para organizar el código
+class Monticulo:
+    def __init__(self):
+        self.listaMonticulo = [0]  
+        self.tamanoActual = 0
+
+    def insertar(self,k):
+        self.listaMonticulo.append(k)
+        self.tamanoActual = self.tamanoActual + 1
+        self.infiltArriba(self.tamanoActual)
+    
+    def infiltArriba(self, i):
+        while i // 2 > 0:
+            if self.listaMonticulo[i] < self.listaMonticulo[i // 2]:
+                self.listaMonticulo[i], self.listaMonticulo[i // 2] = self.listaMonticulo[i // 2], self.listaMonticulo[i]
+            i = i // 2
+        
+    def eliminar_min(self):
+        if self.tamanoActual == 0:
+            return None
+        minimo = self.listaMonticulo[1]
+        self.listaMonticulo[1] = self.listaMonticulo[self.tamanoActual]
+        self.listaMonticulo.pop()
+        self.tamanoActual -= 1
+        self.infiltAbajo(1)
+        return minimo
+
+    def infiltAbajo(self, i):
+        while (i * 2) <= self.tamanoActual:
+            mc = self.hijoMenor(i)
+            if self.listaMonticulo[i] > self.listaMonticulo[mc]:
+                self.listaMonticulo[i], self.listaMonticulo[mc] = self.listaMonticulo[mc], self.listaMonticulo[i]
+            i = mc
+
+    def hijoMenor(self, i):
+        if i * 2 + 1 > self.tamanoActual:
+            return i * 2
+        else:
+            if self.listaMonticulo[i * 2] < self.listaMonticulo[i * 2 + 1]:
+                return i * 2
+            else:
+                return i * 2 + 1
+
+    def minimo(self):
+        if self.tamanoActual == 0:
+            return None
+        return self.listaMonticulo[1]
+
+class ColaDePrioridad:
+    def __init__(self):
+        self.monticulo = Monticulo()
+        self.contador = 0
+
+    def insertar_cola(self, elemento):
+        self.contador += 1
+        self.monticulo.insertar(elemento)
+
+    def eliminar_cola(self):
+        resultado = self.monticulo.eliminar_min()
+        if resultado:
+            return resultado[2]  # Devolver solo el elemento
+        return None
+    
+    def primero(self):
+        minimo = self.monticulo.minimo()
+        if minimo:
+            return minimo[2]
+        return None
